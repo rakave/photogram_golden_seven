241808c2c58da91f79b9a89656c60c56d13f0e63
rakave <velu.ravichandran@chicagobooth.edu>
2015-10-24 16:07:34 -0500

    RCAV Done - Show Completed!

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,20 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    # params =  {"id"=>"2"}
+    @p=Photo.find_by({:id => params[:id] })
+    @photo_id = params[:id]
+    @photo_source = @p.source
+    @photo_caption = @p.caption
+  end
+
+  def edit
+
+  end
+
+  def delete
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos/:id/edit", {:controller => "photos", :action => "edit"})
+  get("/delete_photo/:id", {:controller => "photos", :action => "delete"})
 
 end
